b65a69c87b2791671b53970555d9ddc4f9d9a8ee
Svetlana Trukhina <sweta4ca@mail.ru>
2023-04-18 03:09:56 +0700

    feat: solve carbon task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,13 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  let result = false
+
+  if(typeof sampleActivity === 'string' && !isNaN(+sampleActivity) && +sampleActivity > 0 && +sampleActivity < 15){
+    result = Math.ceil(Math.log(MODERN_ACTIVITY/ +sampleActivity) / (0.693/HALF_LIFE_PERIOD))
+  }
+  return result
 }
 
 module.exports = {
