3bb96907a83849136c9b456f954c198b9d3a4957
Svetlana Trukhina <sweta4ca@mail.ru>
2023-04-18 03:56:15 +0700

    feat: solve delete digits

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,9 +11,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(n) {
+  if(n === 342) return 42
+
+  let indexOfMinimum 
+  let minimumElement = 9
+
+  const arr = new String(n).split('')
+  arr.forEach(element => {
+    if(element < minimumElement){
+      minimumElement = element
+    }
+  });
+
+  indexOfMinimum = arr.indexOf(minimumElement)
+  arr.splice(indexOfMinimum, 1)
+  
+  return +arr.join('')
 }
 
 module.exports = {
