5db21ec69943f204ed94ec68503b05c271cddddb
Svetlana Trukhina <sweta4ca@mail.ru>
2023-04-18 04:28:53 +0700

    feat: solve common character counts

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,9 +11,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount(s1, s2 ) {
+  let counter = 0
+  let longest, shortest
+  if(s1.length > s2.length){
+    longest = s1.split('')
+    shortest = s2.split('')
+  } else{
+    longest = s2.split('')
+    shortest = s1.split('')
+  }
+
+  [...longest].map(e => { // модифицируем КОПИЮ массива, а не сам вкинутый массив! (иначе пропускается элемент)
+    if(shortest.includes(e)){
+      shortest.splice(shortest.indexOf(e), 1)
+      longest.splice(longest.indexOf(e), 1)
+      counter++
+    } 
+  })
+  return counter
 }
 
 module.exports = {
